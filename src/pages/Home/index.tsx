dff4f1d149f7dc3cc8742d66fe129d463b3ced9e
Elian-beep <elian.19batista@gmail.com>
2024-01-08 20:21:07 -0300

    build apk gerado 0.1.2

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,13 +9,14 @@ import Card from "../../components/Card";
 export const Home = ({ navigation }) => {
 
     const [searchProfile, setSearchProfile] = useState<string>();
+    const [strInput, setStrInput] = useState<string>();
 
     return (
         <Content>
             <MainTitle>HUBusca</MainTitle>
             <ContentInpt>
-                <Inpt onChangeText={text => setSearchProfile(text)} placeholder="Perfil a ser buscado" />
-                {/* <Button title="Buscar" /> */}
+                <Inpt onChangeText={text => setStrInput(text)} placeholder="Perfil a ser buscado" />
+                <Button title="Buscar" onPress={() => setSearchProfile(strInput)} />
             </ContentInpt>
            <Card navigation={navigation} person={searchProfile} plus={false} />
         </Content>
